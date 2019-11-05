a64b22cefc4da5442bb3d3d248a1a9f318138cc4
APassionMy <Java@chenbb.cc>
2019-11-05 17:36:21 +0800

    上传github

---
+++ myblog/src/main/java/com/zhy/model/FriendLink.java
@@ -0,0 +1,57 @@
+package com.zhy.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author: zhangocean
+ * @Date: 2019/5/16 14:39
+ * Describe:
+ */
+@Data
+@NoArgsConstructor
+public class FriendLink {
+
+    private int id;
+
+    /**
+     * 博主
+     */
+    private String blogger;
+
+    /**
+     * 博主url
+     */
+    private String url;
+
+    public FriendLink(String blogger, String url){
+        this.blogger = blogger;
+        this.url = url;
+    }
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBlogger() {
+		return blogger;
+	}
+
+	public void setBlogger(String blogger) {
+		this.blogger = blogger;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+    
+
+}
